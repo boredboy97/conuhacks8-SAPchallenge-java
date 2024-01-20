246d5486bbd0f47c2ec5b5ba8301395aefe67a87
notxplosion <greglajoie55@gmail.com>
2024-01-20 15:22:16 -0500

    Create Customer.java

---
+++ WinterTireScheduler/Customer.java
@@ -0,0 +1,16 @@
+public class Customer {
+    private Date dateBooking;
+    private Date dateAppointment;
+
+    public Customer() {
+        
+    }
+    public Customer(Date dateBooking, Date dateAppointment, Vehicle type) {
+        this.dateBooking = null;
+        this.dateAppointment = null;
+        this.type = null;
+    }
+    
+
+     
+}
